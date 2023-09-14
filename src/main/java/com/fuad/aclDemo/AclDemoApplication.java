17f5e930263fc1876c42a32096e7f90580d92dfc
mhfuad <mhfuad2@gmail.com>
2023-09-14 19:00:26 +0600

    first commit

---
+++ src/main/java/com/fuad/aclDemo/AclDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fuad.aclDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclDemoApplication.class, args);
+	}
+
+}
