83362ffd82782357c57f1eb9b2a3f155e5ff0fdd
mhfuad <mhfuad2@gmail.com>
2023-12-04 19:53:18 +0600

    file upload

--- src/main/java/com/fuad/aclDemo/user_info/UserInfoController.java
+++ src/main/java/com/fuad/aclDemo/user_info/UserInfoController.java
@@ -6,7 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileOutputStream;
@@ -14,6 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
@@ -24,7 +27,7 @@ public class UserInfoController {
     private UserRepository userRepository;
     @Autowired
     private UserInfoRepository repository;
-    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads/";
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
 
     //public ResponseObject<UserResponse> registerUser(@Valid @RequestBody UserRegistrationRequest request, BindingResult result){
     @PostMapping("/create")
@@ -43,15 +46,31 @@ public class UserInfoController {
             return ResponseEntity.ok(user);
         }
 
-        byte[] imageBytes = Base64.decodeBase64(request.getImage());
-        try (FileOutputStream fos = new FileOutputStream(UPLOAD_DIRECTORY)){
-            fos.write(imageBytes);
-        } catch (Exception e){
-            e.printStackTrace();
+        try {
+            byte[] decodedBytes = Base64.decodeBase64(request.getImage());
+            String fileName = generateUniqueFileName("profile.jpeg");
+
+            Path directoryPath = Paths.get(UPLOAD_DIRECTORY);
+            if (!directoryPath.toFile().exists()) {
+                directoryPath.toFile().mkdirs();
+            }
+
+            Path filePath = Paths.get(UPLOAD_DIRECTORY, fileName);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(filePath.toFile())) {
+                fileOutputStream.write(decodedBytes);
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            return ResponseEntity.ok("Base64 image decoded and saved successfully. Filename: " + fileName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().body("Error decoding and saving the base64 image."+ HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
 
-        return ResponseEntity.ok("uploads/image.jpg");
-
+    private String generateUniqueFileName(String originalFileName) {
+        return UUID.randomUUID().toString() + "_" + originalFileName;
     }
 
 }
